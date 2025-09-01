f750d89bc0dc88afe7097245ada7922932d78888
HuuVab <90995634+HuuVab@users.noreply.github.com>
2025-09-01 21:24:05 +0700

    as
    as

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
-
-import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
-import 'presentation/providers/todo_provider.dart';
-import 'presentation/pages/todo_list_page.dart';
+import 'providers/note_provider.dart';
+import 'screens/notes_list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +14,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-
       create: (context) => NoteProvider(),
       child: CupertinoApp(
         title: 'Flutter Notes',
@@ -28,16 +23,6 @@ class MyApp extends StatelessWidget {
         ),
         debugShowCheckedModeBanner: false,
         home: const NotesListScreen(),
-
-      create: (context) => TodoProvider(),
-      child: MaterialApp(
-        title: 'Note App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const TodoListPage(),
-
       ),
     );
   }
